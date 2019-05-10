8b7e85797c9c71cbf0c19624634d2cda4b9616ee
ricardoschwarz <schwarz.ricardo.ii@gmail.com>
2019-05-10 13:32:11 +0200

    Added ImageClass and get_dataset to training.py

--- training.py
+++ training.py
@@ -4,12 +4,33 @@ import tensorflow as tf
 from tensorflow.python.framework import ops
 
 
+class ImageClass():
+    def __init__(self, name, image_paths):
+        self.name = name
+        self.image_paths = image_paths
+
+    def __str__(self):
+        return self.name + ', ' + str(len(self.image_paths)) + ' images'
+
+    def __len__(self):
+        return len(self.image_paths)
+
+
 def get_dataset(input_dir):
+    """
+    Builds the dataset from the preprocessed image directory.
+    """
     dataset = []
     classes = os.listdir(input_dir)
     classes.sort()
     
-    # images = ops.convert_to_tensor(input_dir, dtype=tf.string)
+    for c in classes:
+        image_dir = os.path.join(input_dir, c)        
+        if os.path.exists(image_dir):
+            image_names = os.listdir(image_dir)
+            image_paths = [os.path.join(image_dir, image_name) for image_name in image_names]
+            dataset.append(ImageClass(c, image_paths))
+    return dataset
     
 
 if __name__ == "__main__":
